7a8c7a1864e1cedd02136e05e6766a4a8d7c76e3
Alice Peng <zeypen24@bergen.org>
2022-04-02 10:49:50 -0400

    starting

---
+++ change.java
@@ -0,0 +1,20 @@
+//behind the game controls 
+
+/**
+ * Part 1: Sorting Algorithms 
+ * 
+ * Selection Sort
+ * Insertion Sort 
+ * Merge Sort 
+ * 
+ */
+
+/**
+ * Part 2: Searching Algorithms 
+ * 
+ * Linear Search
+ * Binary Search
+ * 
+ */
+
+ 
